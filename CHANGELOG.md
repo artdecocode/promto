3d35b1ea4dfbda330fd5c089ec9181f0cb121877
Anton <anton@adc.sh>
2019-04-29 22:22:31 +0300

    b

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 29 April 2019
+
+### [1.1.0](https://github.com/artdecocode/promto/compare/v1.0.0...v1.1.0)
+
+- [package] Upgrade to 2019; publish the `module` field with `src`.
---
+++ build/index.js
@@ -0,0 +1,44 @@
+function createTimeout(desc, timeout, cb) {
+  return setTimeout(() => {
+    const message = `${desc ? desc : 'Promise'} has timed out after ${timeout}ms`
+    const err = new Error(message)
+    err.stack = err.message
+    cb(err)
+  }, timeout)
+}
+
+function makeTimeoutPromise(desc, timeout) {
+  let to
+  const promise = new Promise((_, reject) => {
+    to = createTimeout(desc, timeout, reject)
+  })
+  return { timeout: to, promise }
+}
+
+/**
+ * Create a promise which will be rejected after a timeout.
+ * @param {!Promise} promise A promise to race with
+ * @param {number} timeout Timeout in ms after which to reject
+ * @param {string} [desc] Description of a promise to be printed in error
+ * @returns {!Promise} A promise with a timeout
+ */
+               async function createPromiseWithTimeout(promise, timeout, desc) {
+  if (!(promise instanceof Promise))
+    throw new Error('Promise expected')
+  if (!timeout)
+    throw new Error('Timeout must be a number')
+  if (timeout < 0)
+    throw new Error('Timeout cannot be negative')
+
+  const { promise: toPromise, timeout: to } = makeTimeoutPromise(desc, timeout)
+  try {
+    return await Promise.race([
+      promise,
+      toPromise,
+    ])
+  } finally {
+    clearTimeout(to)
+  }
+}
+
+module.exports = createPromiseWithTimeout
